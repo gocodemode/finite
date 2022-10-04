8e7c99110a12962976147ac58c521c95b8cd5b57
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-03 20:31:51 -0400

    added entry and output

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,3 +1,11 @@
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
+module.exports = {
+    entry: "./app/index.js", 
+    output: {
+        path: path.resolve(__dirname, "dist"), 
+        filename: "index_bundle.js",
+        publicPath: "/"
+    },
+}
